7d7c714736a334e6c4fe4be87684db0f076c2838
Sushi Yoshi <eito@Macintosh.local>
2022-01-11 09:45:30 +0900

    mitame

--- app.py
+++ app.py
@@ -25,7 +25,6 @@ def index():
 
 @app.route("/graph",methods=['GET', 'POST'])
 def graph():
-    global elements
     try:
         #アクセッション番号からグラフを生成する場合は、GET
         if request.method == 'GET':
@@ -36,6 +35,7 @@ def graph():
             return render_template("loading.html",query=target,depth=depth)
         #アップロードしたjsonファイルからグラフを生成する場合は、POST
         elif request.method == 'POST':
+            global elements
             if 'fileup' not in request.files:
                 return 'file unspecified'
             file = request.files['fileup']
@@ -43,9 +43,13 @@ def graph():
                 return 'file unspecified'
             if file and allwed_file(file.filename):
                 filename = secure_filename(file.filename)
-                #fileはFileStorage型なので、readしてjsonに変換
+                #fileはFileSrage型なので、readしてjsonに変換
+                print(file)
                 elements = json.dumps(json.loads(file.read()))
+                print(elements)
                 return render_template("graph.html",elem=elements,filename=filename)
+            else:
+                return 'file error'
         else:
             return abort(400)
     except Exception as e:
@@ -53,12 +57,14 @@ def graph():
 
 @app.route("/analyze",methods=['GET', 'POST'])
 def analyze():
+    global elements
     #アクセッション番号
     target = request.args.get('query', '')
     #深さ
     depth = request.args.get('depth', '')
     filename = target + '.json'
     elements = xml_to_json.xmlTojson_fileoutput(target,int(depth))
+    print(elements)
     #無効なアクセッション番号
     if elements == -1:
         return 'Invalid accession number'
@@ -71,6 +77,7 @@ def donwload():
     filename = request.args.get('button', '')
     #ファイルをサーバ内に残さないために、インメモリでjsonファイルの内容を展開
     mem = io.BytesIO()
+    print(elements)
     mem.write( elements.encode('utf-8'))
     mem.seek(0)
     #ユーザーに処理したjsonファイルを送信する
