1811567ba83e1f42fe029e43318565191d53c2c1
Sushi Yoshi <eito@Macintosh.local>
2022-01-26 09:59:04 +0900

    file upload2

--- app.py
+++ app.py
@@ -10,10 +10,11 @@ import tempfile
 import io
 
 app = Flask(__name__)
+global elements
+elements = {}
 
 # アップロードされる拡張子の制限
 ALLOWED_EXTENSIONS = set(['json'])
-
 def allwed_file(filename):
     # .があるかどうかのチェックと、拡張子の確認
     # OKなら１、だめなら0
@@ -21,10 +22,42 @@ def allwed_file(filename):
 
 @app.route("/")
 def index():
+    print(elements)
     return render_template("index.html")
 
-@app.route("/graph",methods=['GET', 'POST'])
-def graph():
+# @app.route("/graph",methods=['GET', 'POST'])
+# def graph():
+#     try:
+#         #アクセッション番号からグラフを生成する場合は、GET
+#         if request.method == 'GET':
+#             #アクセッション番号
+#             target = request.args.get('query', '')
+#             #深さ
+#             depth = request.args.get('depth', '')
+#             return render_template("loading.html",query=target,depth=depth)
+#         #アップロードしたjsonファイルからグラフを生成する場合は、POST
+#         elif request.method == 'POST':
+#             global elements
+#             if 'fileup' not in request.files:
+#                 return 'file unspecified'
+#             file = request.files['fileup']
+#             if file.filename == '':
+#                 return 'file unspecified'
+#             if file and allwed_file(file.filename):
+#                 filename = secure_filename(file.filename)
+#                 #fileはFileSrage型なので、readしてjsonに変換
+#                 elements[filename] = json.dumps(json.loads(file.read()))
+#                 print(elements[filename])
+#                 return render_template("graph.html",elem=elements[filename],filename=filename)
+#             else:
+#                 return 'file unspecified'
+#         else:
+#             return abort(400)
+#     except Exception as e:
+#         return str(e)
+
+@app.route("/analyze_mode",methods=['GET'])
+def analyze_mode():
     try:
         #アクセッション番号からグラフを生成する場合は、GET
         if request.method == 'GET':
@@ -33,29 +66,12 @@ def graph():
             #深さ
             depth = request.args.get('depth', '')
             return render_template("loading.html",query=target,depth=depth)
-        #アップロードしたjsonファイルからグラフを生成する場合は、POST
-        elif request.method == 'POST':
-            global elements
-            if 'fileup' not in request.files:
-                return 'file unspecified'
-            file = request.files['fileup']
-            if file.filename == '':
-                return 'file unspecified'
-            if file and allwed_file(file.filename):
-                filename = secure_filename(file.filename)
-                #fileはFileSrage型なので、readしてjsonに変換
-                print(file)
-                elements = json.dumps(json.loads(file.read()))
-                print(elements)
-                return render_template("graph.html",elem=elements,filename=filename)
-            else:
-                return 'file error'
         else:
-            return abort(400)
+            return render_template("error.html",error_text=abort(400))
     except Exception as e:
-        return str(e)
+        return render_template("error.html",error_text=(str(e)))
 
-@app.route("/analyze",methods=['GET', 'POST'])
+@app.route("/analyze",methods=['GET'])
 def analyze():
     global elements
     #アクセッション番号
@@ -63,25 +79,54 @@ def analyze():
     #深さ
     depth = request.args.get('depth', '')
     filename = target + '.json'
-    elements = xml_to_json.xmlTojson_fileoutput(target,int(depth))
-    print(elements)
+    elements[filename] = xml_to_json.xmlTojson_fileoutput(target,int(depth))
     #無効なアクセッション番号
-    if elements == -1:
-        return 'Invalid accession number'
-    return render_template("graph.html",elem=elements,filename=filename)
+    if elements[filename] == -1:
+        return render_template("error.html",error_text='Invalid accession number')
+    return render_template("graph.html",elem=elements[filename],filename=filename)
 
+@app.route("/file_mode",methods=['POST'])
+def file_mode():
+    try:
+    #アップロードしたjsonファイルからグラフを生成する場合は、POST
+        if request.method == 'POST':
+            depth = request.form.get("depth")
+            global elements
+            if 'fileup' not in request.files:
+                return render_template("error.html",error_text='file unspecified')
+            file = request.files['fileup']
+            if file.filename == '':
+                return render_template("error.html",error_text='file unspecified')
+            if file and allwed_file(file.filename):
+                filename = secure_filename(file.filename)
+                #fileはFileSrage型なので、readしてjsonに変換
+                json_file = json.dumps(json.loads(file.read()))
+                depth = int(depth)
+                if depth != 0:
+                    json_file = xml_to_json.xmlTojson_inp_json(json_file,depth)
+                elements[filename] = json_file
+                #print(elements[filename])
+                return render_template("graph.html",elem=elements[filename],filename=filename)
+            else:
+                return render_template("error.html",error_text='file unspecified')
+        else:
+            return abort(400)
+    except Exception as e:
+        return str(e)
 
 @app.route('/download',methods=['GET','POST'])
 def donwload():
     #ファイル名はダウンロードボタンに記載してある
     filename = request.args.get('button', '')
-    #ファイルをサーバ内に残さないために、インメモリでjsonファイルの内容を展開
-    mem = io.BytesIO()
-    print(elements)
-    mem.write( elements.encode('utf-8'))
-    mem.seek(0)
-    #ユーザーに処理したjsonファイルを送信する
-    return send_file( mem, mimetype='application/json', as_attachment=True, attachment_filename=filename)
-
+    file = elements[filename]
+    if file:
+        #ファイルをサーバ内に残さないために、インメモリでjsonファイルの内容を展開
+        mem = io.BytesIO()
+        mem.write( file.encode('utf-8'))
+        mem.seek(0)
+        #ユーザーに処理したjsonファイルを送信する
+        return send_file( mem, mimetype='application/json', as_attachment=True, attachment_filename=filename)
+    else:
+        return render_template("error.html",error_text='This file may have been invalidated because the timeout period has elapsed.')
 if __name__ == "__main__":
     app.run(debug=True)
--- xml_to_json.py
+++ xml_to_json.py
@@ -3,6 +3,8 @@ from lxml import etree
 import json
 import html
 import sys
+import re
+import time
 
 def getxmlRoot(target):
 	#入力されたアクセッション番号から、起点となるxmlをuniprotから読み込む。
@@ -13,18 +15,52 @@ def getxmlRoot(target):
 	return etree.fromstring(response.content)
 
 def xmlTojson_fileoutput(target,depth):
+	start = time.time()
 	root = getxmlRoot(target)
 	if root == -1:
 		return -1
 	name = root.find('entry/name',root.nsmap).text
 	#このdict_がのちにjsonとして返される。
-	dict_ = [{"group":"nodes","classes":"root","data":{"name":name,"id":name}}]
+	dict_ = [{"end_num":"","classes":"pydata","comp":{}},{"group":"nodes","classes":"root","data":{"name":name,"id":name}}]
 	#すでに相互作用の関係が検出されたタンパク質を記憶しておく
-	completion_dict = {name:[]}
+	completion_dict = {"node_num":0,name:["f"]}
 	findInteractaion(root,name,0,dict_,depth,completion_dict)
+	dict_[0]['comp'] = completion_dict
 	print(target + ".json done")
 	print(completion_dict)
-	return json.dumps(dict_)
+	print(dict_)
+	# arr = dict_[0]['end_num'].split(',')
+	# del arr[-1]
+	# numarray = list(map(int,arr))
+	# print(numarray)
+	# for index in numarray:
+	# 	print(dict_[index]['data']['name'])
+	end = time.time()
+	print(format(end-start))
+	return json.dumps(dict_,ensure_ascii=True,indent=2)
+
+def xmlTojson_inp_json(json_file,depth):
+	start = time.time()
+	dict_ = json.loads(str(json_file))
+	arr = dict_[0]['end_num'].split(',')
+	del arr[-1]
+	numarray = list(map(int,arr))
+	completion_dict = dict_[0]["comp"]
+	print(completion_dict)
+	#print(dict_[numarray[1]]['data']['name'])
+	for index in numarray:
+		target = dict_[index]['ac_num']
+		print(target)
+		root = getxmlRoot(target)
+		if root == -1:
+			return -1
+		name = dict_[index]['data']['name']
+		#completion_dict[name] = []
+		findInteractaion(root,name,0,dict_,int(depth),completion_dict)
+	print(dict_)
+	end = time.time()
+	print(format(end-start))
+	return json.dumps(dict_,ensure_ascii=True,indent=2)
 
 def findInteractaion(root,name,n,dict_,depth,completion_dict):
 	#type属性がinteractionになっているタグを検出
@@ -38,36 +74,45 @@ def findInteractaion(root,name,n,dict_,depth,completion_dict):
 	for elem in comment:
 		#interactantには、自分自身のアクセッション番号も記録されている
 		intactList = [intact for intact in elem.findall('interactant',root.nsmap)]
-		for intact in intactList:
-			_id = intact.find('id',root.nsmap)
-			#遷移先のタンパク質のxmlファイルを、アクセッション番号(_id.text)から取得する
-			child_root = getxmlRoot(_id.text)
-			if child_root == -1:
-				continue
-			child_name = child_root.find('entry/name',root.nsmap).text
-			#親と子の名前が一緒ではないなら
-			if child_name != name:
-				edges_num = edges_num + 1
-				#もし解析が完了していないのなら
-				if child_name not in completion_dict[name]:
-					for i in range(_n):
-						print('  ',end='')
-					print(child_name)
-					#子ノードを追加
-					node = {"group":"nodes","data":{"name":child_name,"id":child_name}}
-					#親ノードと子ノードを接続
-					edges = {"group":"edges","data":{"id":name+str(edges_num),"source":name,"target":child_name}}
-					#jsonに追加
-					dict_ += [node,edges]
-					#親ノードを起点とした相互作用関係リストに子ノードの相互関係を追加
-					completion_dict[name] += [child_name]
-				#ユーザが指定した深さより小さい　かつ まだ子ノードの解析が完了していない
-				if _n < depth and _id.text not in completion_dict:
-					#子ノードを起点とした相互作用関係リストを新たに作成
-					completion_dict[child_name] = [name,child_name]
-					#子ノードを起点に新たに解析を開始
-					findInteractaion(child_root,child_name,_n,dict_,depth,completion_dict)
+		#intactList[0]には、常に自分自身のアクセッション番号が入っている
+		intact=intactList[1]
+		_id = intact.find('id',root.nsmap)
 
+		#遷移先のタンパク質のxmlファイルを、アクセッション番号(_id.text)から取得する
+		child_root = getxmlRoot(_id.text)
+		if child_root == -1:
+			continue
+		child_name = child_root.find('entry/name',root.nsmap).text
+		#親と子の名前が一緒ではないなら
+		if child_name != name:
+			edges_num = edges_num + 1
+			#もし解析が完了していないのなら
+			#completion_dict[name] = []
+
+			if child_name not in completion_dict[name]:
+				completion_dict['node_num'] += 2
+				for i in range(_n):
+					print('  ',end='')
+				print(child_name)
+				#子ノードを追加
+				node = {"group":"nodes","data":{"name":child_name,"id":child_name},"ac_num":_id.text}
+				#親ノードと子ノードを接続
+				edges = {"group":"edges","data":{"id":name+str(edges_num),"source":name,"target":child_name}}
+				#jsonに追加
+				dict_ += [node,edges]
+				#親ノードを起点とした相互作用関係リストに子ノードの相互関係を追加
+				completion_dict[name] += [child_name]
+				completion_dict[child_name] = ["t",name,child_name]
+			#ユーザが指定した深さより小さい　かつ まだ子ノードの解析が完了していない
+			#if _n < depth and child_name not in completion_dict:
+			if _n < depth and completion_dict[child_name][0] == "t":
+				#子ノードを起点とした相互作用関係リストを新たに作成
+				#completion_dict[child_name] = [name,child_name]
+				completion_dict[child_name][0] = "f"
+				#子ノードを起点に新たに解析を開始
+				findInteractaion(child_root,child_name,_n,dict_,depth,completion_dict)
+			else:
+				dict_[0]['end_num'] += str(completion_dict['node_num']) + ','
 if __name__ == '__main__':	
 	#デバッグ用 アクセッション番号と深さをコマンド引数に取る。
 	target = str(sys.argv[1])
